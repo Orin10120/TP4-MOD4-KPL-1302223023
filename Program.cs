f70406ed75dde9f389b67df10f5bdd1c64e7185a
Orin10120 <rindang3001@gmail.com>
2024-03-10 11:58:35 +0700

    Finish TP

--- Program.cs
+++ Program.cs
@@ -10,9 +10,9 @@ namespace Modul4_1302223023
     {
         static void Main(string[] args)
         {
-            /*Console.WriteLine("Kode Pos Dari " + KodePos.Kelurahan.Kujangsari + " adalah " + KodePos.getKodePos(KodePos.Kelurahan.Kujangsari));
+            Console.WriteLine("Kode Pos Dari " + KodePos.Kelurahan.Kujangsari + " adalah " + KodePos.getKodePos(KodePos.Kelurahan.Kujangsari));
             Console.WriteLine("Kode Pos Dari " + KodePos.Kelurahan.Margasari + " adalah " + KodePos.getKodePos(KodePos.Kelurahan.Margasari));
-            Console.WriteLine("Kode Pos Dari " + KodePos.Kelurahan.Batununggal + " adalah " + KodePos.getKodePos(KodePos.Kelurahan.Batununggal));*/
+            Console.WriteLine("Kode Pos Dari " + KodePos.Kelurahan.Batununggal + " adalah " + KodePos.getKodePos(KodePos.Kelurahan.Batununggal));
 
 
             DoorMachine machine = new DoorMachine();
