34d78ba753c8809d33e7b83bb7e8309e567b9a05
Orin10120 <rindang3001@gmail.com>
2024-03-10 11:35:13 +0700

    Finish TP

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modul4_1302223023
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kode Pos Dari " + KodePos.Kelurahan.Kujangsari + " adalah " + KodePos.getKodePos(KodePos.Kelurahan.Kujangsari));
+            Console.WriteLine("Kode Pos Dari " + KodePos.Kelurahan.Margasari + " adalah " + KodePos.getKodePos(KodePos.Kelurahan.Margasari));
+            Console.WriteLine("Kode Pos Dari " + KodePos.Kelurahan.Batununggal + " adalah " + KodePos.getKodePos(KodePos.Kelurahan.Batununggal));
+
+
+            /*DoorMachine machine = new DoorMachine();
+            machine.RunState(); */
+        }
+    }
+}
