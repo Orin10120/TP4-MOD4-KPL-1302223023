693c254e36cb7c60b22e5f716b3e6532d64898c4
Orin10120 <rindang3001@gmail.com>
2024-03-10 11:27:52 +0700

    menambahkan implementasi dengan table-driven

---
+++ KodePos.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modul4_1302223023
+{
+    public class KodePos
+    {
+        public enum Kelurahan { Batununggal, Kujangsari, Mengger, Wates, Cijaura, Jatisari, Margasari, Sekejati, Kebonwaru, Maleer, Samoja}
+
+        public static int getKodePos(Kelurahan kelurahan)
+        {
+            int[] KodePos = { 40266, 40287, 40267, 40256, 40287, 40286, 40286, 40272, 40274, 40273 };
+            return KodePos[(int) kelurahan];
+        }
+    }
+}
